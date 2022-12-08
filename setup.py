6665b7f863b0f6f67eebebf9137a9ffcc09c86ca
rodolphomacedo <rodolpho.ime@gmail.com>
2022-12-08 15:28:55 -0300

    update: summary function work with multiple index
    The function Vide.summary() is update. Now this allowed working
    with a parameters with index.
    
    For example:
    
        - alpha[0] and alpha[1]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='videpy',
-    version='0.0.9',
+    version='0.0.10',
     license='MIT License',
     author='Rodolpho Macedo dos Santos',
     long_description='Useful Bayesian tools to analysis and visualization of the samples. ',
