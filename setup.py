2c669d6f89baa9e572ef880cad8ca70a3e527ab3
rodolphomacedo <rodolpho.ime@gmail.com>
2022-12-08 15:16:09 -0300

    Merge conflicts

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from setuptools import setup
 
 setup(
     name='videpy',
-<<<<<<< HEAD
     version='0.0.9',
-=======
-    version='0.0.8',
->>>>>>> bb8304cc1b8cfda2fe3a19e7b2dc0f690984c538
     license='MIT License',
     author='Rodolpho Macedo dos Santos',
     long_description='Useful Bayesian tools to analysis and visualization of the samples. ',
